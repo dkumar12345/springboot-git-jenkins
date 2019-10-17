54ccffec920333cd9fe5b253ca788731f2d377d4
localadmin <localadmin@INBLWFLP015554.infotech.com>
2019-10-17 08:17:59 +0530

    added new method in application class

--- dk-dev-demo/src/main/java/com/example/dkdevdemo/DkDevDemoApplication.java
+++ dk-dev-demo/src/main/java/com/example/dkdevdemo/DkDevDemoApplication.java
@@ -2,12 +2,21 @@ package com.example.dkdevdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DkDevDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DkDevDemoApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String getMessage() {
+		
+		return "Welcome to springboot git jenkins application";
 	}
 
 }
