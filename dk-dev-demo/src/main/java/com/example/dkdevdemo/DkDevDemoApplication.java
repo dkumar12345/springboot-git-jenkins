1968b0fdf2968e74ed167f417c4332a5ec2d8407
localadmin <localadmin@INBLWFLP015554.infotech.com>
2019-10-17 08:00:53 +0530

    added new project

---
+++ dk-dev-demo/src/main/java/com/example/dkdevdemo/DkDevDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dkdevdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkDevDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkDevDemoApplication.class, args);
+	}
+
+}
